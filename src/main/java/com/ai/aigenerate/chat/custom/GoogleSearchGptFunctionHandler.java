68c8f56fd55aa72c9b1b530c634b45b42f69d84f
李一凡 <yifan.li@xtransfer.cn>
2024-02-21 13:51:54 +0800

    add search plugin

--- src/main/java/com/ai/aigenerate/chat/custom/GoogleSearchGptFunctionHandler.java
+++ src/main/java/com/ai/aigenerate/chat/custom/GoogleSearchGptFunctionHandler.java
@@ -3,7 +3,7 @@ package com.ai.aigenerate.chat.custom;
 
 import cn.hutool.json.JSONObject;
 import com.ai.aigenerate.chat.AbstractGptFunctionHandler;
-import com.ai.aigenerate.chat.tool.BaiduSearchService;
+import com.ai.aigenerate.chat.tool.GoogleSearchService;
 import com.ai.aigenerate.model.request.baidu.SearchRequest;
 import com.alibaba.fastjson2.JSON;
 import com.unfbx.chatgpt.entity.chat.Functions;
@@ -11,22 +11,25 @@ import com.unfbx.chatgpt.entity.chat.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
 
 @Component
-public class BaiduSearchGptFunctionHandler extends AbstractGptFunctionHandler<SearchRequest> {
+public class GoogleSearchGptFunctionHandler extends AbstractGptFunctionHandler<SearchRequest> {
 
     @Autowired
-    private BaiduSearchService baiduSearchService;
+    private GoogleSearchService googleSearchService;
 
-    @Value("${function.baidu.search.desc:通过百度进行搜索,搜索结果以json格式返回}")
-    private String baiduSearchFunctionDesc;
+    @Value("${function.google.search.desc:" +
+            "当需要使用谷歌搜索实时信息或资讯有关的问题会通过意图识别决策到该插件，使用 \"搜索\" 或 \"查询\" 等关键词可以提高命中率\n" +
+            "You need to fully understand user needs and provide as complete and accurate search keywords as possible. \n" +
+            "Separate multiple keywords with spaces.  \n" +
+            "Please avoid providing any extra text, so that I can directly pass the keywords to the search engine，搜索结果以json格式返回}")
+    private String googleSearchFunctionDesc;
 
     @Override
     public String doHandle(String paramJson) {
         SearchRequest searchRequest = JSON.parseObject(paramJson, SearchRequest.class);
-        return baiduSearchService.getBaiduSearchResult(searchRequest.getKeyword());
+        return googleSearchService.googleSearch(searchRequest.getKeyword());
     }
 
     @Override
@@ -36,15 +39,15 @@ public class BaiduSearchGptFunctionHandler extends AbstractGptFunctionHandler<Se
         keyword.putOpt("description", "查询的关键字,参数中不允许出现空格");
 
         //参数
-        JSONObject properties = new JSONObject();
+        JSONObject properties = new cn.hutool.json.JSONObject();
         properties.putOpt("keyword", keyword);
         Parameters parameters = Parameters.builder()
                 .type("object")
                 .properties(properties)
                 .required(Arrays.asList("keyword")).build();
         Functions functions = Functions.builder()
-                .name("baiduSearch")
-                .description(baiduSearchFunctionDesc)
+                .name("googleSearch")
+                .description(googleSearchFunctionDesc)
                 .parameters(parameters)
                 .build();
         return functions;
